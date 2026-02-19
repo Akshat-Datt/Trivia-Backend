9034fd533c1988294955d4c6861fcd72c58a486c
Akshat-Datt <dattakshat31@gmail.com>
2026-02-20 00:56:04 +0530

    creating a basic route handler and running the server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app: Router<()> = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    println!("Server running at http://{}", addr);
+
+    axum::serve(
+        tokio::net::TcpListener::bind(addr).await.unwrap()
+        ,app
+    )
+    .await.unwrap();
+}
+
+async fn root() -> &'static str{
+    "Trivia backend running"
 }
