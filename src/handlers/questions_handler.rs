8dcd75deaec0e7289e3b87025cec879380e6ca2f
Akshat-Datt <dattakshat31@gmail.com>
2026-03-14 11:01:46 +0530

    replacing truncate with sql query in get questions handler

--- src/handlers/questions_handler.rs
+++ src/handlers/questions_handler.rs
@@ -6,14 +6,22 @@ use crate::{models::question_data::{CreateQuestion, Question, QuestionQuery}};
 use crate::state::app_state::AppState;
 
 pub async fn get_questions(Query(params): Query<QuestionQuery>, State(state): State<AppState>) -> Result<Json<Vec<Question>>, StatusCode>{
-    let mut questions = sqlx::query_as::<_, Question>("SELECT id, question FROM questions")
+
+    
+    let questions = if let Some(limit) = params.limit{
+    sqlx::query_as::<_, Question>("SELECT id, question FROM questions LIMIT $1")
+        .bind(limit as i64)
         .fetch_all(&state.db)
         .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    if let Some(limit) = params.limit{
-        questions.truncate(limit);
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
     }
+    
+    else{
+    sqlx::query_as::<_, Question>("SELECT id, question FROM questions")
+        .fetch_all(&state.db)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+    };
 
     Ok(Json(questions))
 }
