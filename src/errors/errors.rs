7f256305d8a4744175fb6e386d78362992b5fd90
Akshat-Datt <dattakshat31@gmail.com>
2026-03-29 21:14:24 +0530

    errors changed from string to json format with serde

--- src/errors/errors.rs
+++ src/errors/errors.rs
@@ -1,34 +1,37 @@
 use axum::{
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    Json
+    Json, http::StatusCode, response::{IntoResponse, Response}
 };
+use serde::Serialize;
 
 #[derive(Debug)]
 pub enum AppError{
     ValidationError(String),
     NotFound(String),
-    Deleted,
     DatabaseError
 }
+
+#[derive(Serialize)]
+struct ErrorResponse{
+    error: String
+}
+
+
 
 impl IntoResponse for AppError{
     fn into_response(self) -> Response {
         match self {
             AppError::ValidationError(msg) => {
-                (StatusCode::BAD_REQUEST, Json(msg)).into_response()
+                let body = ErrorResponse {error : msg};
+                (StatusCode::BAD_REQUEST, Json(body)).into_response()
             },
 
             AppError::NotFound(msg) => {
-                (StatusCode::NOT_FOUND, Json(msg)).into_response()
+                let body = ErrorResponse {error : msg};
+                (StatusCode::NOT_FOUND, Json(body)).into_response()
             },
-
-            AppError::Deleted => {
-                (StatusCode::NO_CONTENT, Json("Resource deleted".to_string())).into_response()
-            },
-
             AppError::DatabaseError => {
-                (StatusCode::INTERNAL_SERVER_ERROR, Json("Something went wrong".to_string())).into_response()
+                let body = ErrorResponse {error : "Something went wrong".to_string()};
+                (StatusCode::INTERNAL_SERVER_ERROR, Json(body)).into_response()
             }
         }
     }
