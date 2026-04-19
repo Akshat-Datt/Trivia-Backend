063ac3aed726bde49b64be9008aa5d5845635d55
Akshat-Datt <dattakshat31@gmail.com>
2026-04-19 21:50:27 +0530

    chnaging address soo url is available to other devices as well

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ async fn main() {
         .with_state(shared_state);
 
     axum::serve(
-        tokio::net::TcpListener::bind("127.0.0.1:3000")
+        tokio::net::TcpListener::bind("0.0.0.0:3000")
             .await
             .unwrap(),
         app,
